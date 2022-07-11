c8b1cbb278b784a4f80aeb59e8fd5e6da7faa4b1
Daniel Kwon <skansmfqh7312@gmail.com>
2022-07-11 17:04:11 +0900

    StackView 활용한 Layout 구성

--- SeSACWeek2/ViewController.swift
+++ SeSACWeek2/ViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var greenView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        greenView.layer.cornerRadius = 20
+        greenView.clipsToBounds = true // shadow
+        //greenView.layer.shadow 활용
+        //clipsToBounds vs cornerRadius vs shadow
+        // label image view를 깎아 보면서 정리해보자
+        
+        
+        greenView.layer.cornerRadius = greenView.frame.width / 2
+        //모서리 한쪽만 원형
+        greenView.layer.maskedCorners = [.layerMinXMaxYCorner]
+        greenView.layer.cornerRadius = 10
+        
     }
 
 
