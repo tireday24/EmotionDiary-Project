8033ebb43536d4791ea99740d850fe2d1febcd34
Daniel Kwon <skansmfqh7312@gmail.com>
2022-07-11 18:03:26 +0900

    Autolayout 설정

---
+++ SeSACWeek2/EmotionDiaryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EmotionDiaryViewController.swift
+//  SeSACWeek2
+//
+//  Created by 권민서 on 2022/07/11.
+//
+
+import UIKit
+
+class EmotionDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
