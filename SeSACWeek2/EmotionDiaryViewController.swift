80d96f21d9ce6369b40554a309e8b7ede680e3a9
Daniel Kwon <skansmfqh7312@gmail.com>
2022-07-11 21:19:57 +0900

    코딩.. 해결.. 재밌다..(7시간 ㅋㅋ) Unbutton, UILabel 버튼 누를 때 마다 Label 숫자 1 증가, Action 모음 만들어서 최대한 반복 줄임, Substring -> String -> Int

--- SeSACWeek2/EmotionDiaryViewController.swift
+++ SeSACWeek2/EmotionDiaryViewController.swift
@@ -8,22 +8,107 @@
 import UIKit
 
 class EmotionDiaryViewController: UIViewController {
-
+    
+    @IBOutlet weak var happyButton: UIButton!
+    @IBOutlet weak var loveButton: UIButton!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var stuffyButton: UIButton!
+    @IBOutlet weak var upsetButton: UIButton!
+    @IBOutlet weak var gloomyButton: UIButton!
+    @IBOutlet weak var boredButton: UIButton!
+    @IBOutlet weak var tiredButton: UIButton!
+    @IBOutlet weak var giveUpButton: UIButton!
+    
+    
+    @IBOutlet weak var happyLabel: UILabel!
+    @IBOutlet weak var loveLabel: UILabel!
+    @IBOutlet weak var likeLabel: UILabel!
+    @IBOutlet weak var stuffyLabel: UILabel!
+    @IBOutlet weak var upsetLabel: UILabel!
+    @IBOutlet weak var gloomyLabel: UILabel!
+    @IBOutlet weak var boredLabel: UILabel!
+    @IBOutlet weak var tiredLabel: UILabel!
+    @IBOutlet weak var giveUpLabel: UILabel!
+    
+    // data source
+    var dictionary = [UIButton: UILabel]() // [:]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // dictionary 를 채워주기
+        dictionary = [happyButton: happyLabel, loveButton: loveLabel, likeButton: likeLabel, stuffyButton: stuffyLabel, upsetButton: upsetLabel, gloomyButton: gloomyLabel, boredButton: boredLabel, tiredButton: tiredLabel, giveUpButton: giveUpLabel ]
+        
+        
+        labelDesign(happyLabel, labelTitle: "행복해")
+        labelDesign(loveLabel, labelTitle: "사랑해")
+        labelDesign(likeLabel, labelTitle: "좋아해")
+        labelDesign(stuffyLabel, labelTitle: "당황해")
+        labelDesign(upsetLabel, labelTitle: "속상해")
+        labelDesign(gloomyLabel, labelTitle: "우울해")
+        labelDesign(boredLabel, labelTitle: "심심해")
+        labelDesign(tiredLabel, labelTitle: "따분해")
+        labelDesign(giveUpLabel, labelTitle: "포기해")
+    }
+    
+    func labelDesign(_ labelName : UILabel, labelTitle: String) {
+        labelName.layer.backgroundColor = UIColor.clear.cgColor
+        labelName.font = .systemFont(ofSize: 20)
+    }
+    
+    
+    /*
+     행복해 4
+     1. label1: 행복해 2
+        -> split ->  행복해 / 100 [행복해, -100]
+        -> array[1]
+            강제 / 확인
+        -> Int(array[1]) -> optional
+        -> 벗겨지면, 숫자 + 1
+        -> array[0] + " " + \(+1 한숫자)
+     2. label1: 행복해 label2: 4 -> layout X
+     */
+    
+    // [uibutton: uilabel] <- sender 를 사용해서 uilabel
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        // sender 에 맞는 label을 찾아주기 dictionary 사용해서
+        if let label = dictionary[sender] {
+            if let labelTexts = label.text?.split(separator: " ") { // [행복해] count = 1
+                if labelTexts.count == 2 {
+                    // array 의 index 1 에 접근해서 숫자 빼내기
+                    // 만약, array 의 element 갯수가 1개면, 못빼옴. crash
+                    // 빼오려면, 반드시  array count가 2여야함.
+                    let number = labelTexts[1]
+                    if var num = Int(String(number)) { // 숫자로 변환 가능하면
+                        num += 1
+                        label.text = String(labelTexts[0]) + " \(num)"
+                        // assign new label to happyLabel's text
+                    }
+                } else if labelTexts.count == 1 {
+                    // 행복해 1
+                    // happyLabel.text에 1을 추가로 넣어준다
+                    if let text = label.text {
+                        label.text = text + " 1"
+                    }
+                }
+            }
+        }
+        
     }
     
 
-    /*
-    // MARK: - Navigation
+   
+    
+    
+       
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
